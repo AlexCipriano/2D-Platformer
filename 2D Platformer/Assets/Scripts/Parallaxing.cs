a4d51726217c4b9edaa7fc61ed29a6e24f7935d4
AlexCipriano <aciprian@uncc.edu>
2017-11-01 14:20:17 -0400

    parallaxing finished
    Finished parallaxing script, added to game manager.

--- 2D Platformer/Assets/Scripts/Parallaxing.cs
+++ 2D Platformer/Assets/Scripts/Parallaxing.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Parallaxing : MonoBehaviour {
 
-    public Transform[]
+    public Transform[] backgrounds;
+    private float[] parallaxScales;
+    public float smoothing = 1;
 
-	// Use this for initialization
-	void Start () {
+    private Transform cam;
+    private Vector3 previousCamPosition;
+
+    private void Awake() {
+        cam = Camera.main.transform;
+    }
+
+    // Use this for initialization
+    void Start () {
+        //store the previous frame
+        previousCamPosition = cam.position;
+        parallaxScales = new float[backgrounds.Length];
+
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z * -1;
+        }
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		for (int i = 0; i < backgrounds.Length; i++)
+        {
+            float parallax = (previousCamPosition.x - cam.position.x) * parallaxScales[i];
+
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+
+
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
+
+        previousCamPosition = cam.position;
+    }
 }
+
