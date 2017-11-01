51f61dba89c761cd203c114fbc60f23e8201ec6e
Austin Dean <adean31@uncc.edu>
2017-11-01 14:12:43 -0400

    Added hub object, not implemented

---
+++ 2D Platformer/Assets/Scripts/Player_Hud.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Hud : MonoBehaviour {
+
+    public Slider healthBar;
+    public Text endGameText;
+    public bool isGameOver;
+
+	// Use this for initialization
+	void Start () {
+        endGameText.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isGameOver)
+            transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 10f, 0, 0);
+    }
+
+}
